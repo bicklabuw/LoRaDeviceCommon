8760aa728db9765acf1df56edc29e72b4bb98d40
Ian Barbour <ibarbour@wisc.edu>
2025-12-05 17:53:15 -0600

    Working stuff here

--- PacketConfig.h
+++ PacketConfig.h
@@ -1,41 +1,37 @@
 #ifndef PACKET_CONFIG_H
 #define PACKET_CONFIG_H
 
-// --- LORA SETTINGS ---
+// --- LORA RADIO SETTINGS ---
 #define RF_FREQUENCY        915000000 // Hz
 #define TX_OUTPUT_POWER     14        // dBm
 #define LORA_BANDWIDTH      0         // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
 #define LORA_CODINGRATE     1         // [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
-#define LORA_PREAMBLE_LEN   8         
-#define LORA_SYMBOL_TIMEOUT 0         
+#define LORA_PREAMBLE_LEN   8
+#define LORA_SYMBOL_TIMEOUT 0
 #define LORA_FIX_LENGTH_PAYLOAD_ON false
 #define LORA_IQ_INVERSION_ON false
 
-// --- TIMING ---
-#define RX_TIMEOUT_VALUE    2000
-#define PACKET_WAIT_DELAY   500      // Delay between SF test packets
-#define DISCOVERY_TIMEOUT   5000     // How long to listen for discovery responses
-#define BACKOFF_MAX_DELAY   2000     // Max random delay for nodes to reply to discovery
+// --- TIMING CONFIGURATION ---
+#define RX_TIMEOUT_VALUE    2000   // Hardware timeout (ms)
+#define PACKET_WAIT_DELAY   500    // Delay between generic packets
+#define DISCOVERY_TIMEOUT   5000   // Time (ms) to listen for responses after a scan request
+#define BACKOFF_MAX_DELAY   2000   // Max random delay (ms) for nodes to reply to avoid collisions
+
+// --- ADDRESS DEFINITIONS ---
+#define BASE_STATION_ID     0x00
+#define BROADCAST_ADDR      0xFF
 
 // --- PACKET TYPES ---
-#define PKT_DISCOVERY_REQ   0x01 // "Who is out there?"
-#define PKT_DISCOVERY_RESP  0x02 // "I am here, ID: X"
-#define PKT_SF_TEST         0x03 // SF Test Packet
-#define PKT_SF_ACK          0x04 // SF Test Acknowledgement
-#define PKT_CMD_SEARCH      0x05 // "Node X, search your neighbors now"
-#define PKT_REPORT_NODE     0x06 // "Base, I found Node Y with Capacity Z"
-#define PKT_SEARCH_DONE     0x07 // "Base, I finished searching"
+// 1. Discovery Phase
+#define PKT_DISCOVERY_REQ   0x01  // "Who is out there?" (Broadcast or Direct)
+#define PKT_DISCOVERY_RESP  0x02  // "I am here, ID: X" (Response to REQ)
 
-// --- ADDRESSING ---
-#define BROADCAST_ADDR      0xFF
-#define BASE_STATION_ID     0x00
+// 2. Link Testing (Optional/Future)
+#define PKT_SF_TEST         0x03  // SF Test Packet
 
-// --- PACKET STRUCTURE ---
-// [0]: Destination ID
-// [1]: Sender ID
-// [2]: Packet Type
-// [3]: Data 1 (e.g., SF level or target ID)
-// [4]: Data 2 (e.g., RSSI or Capacity)
-// [5+]: Payload (Optional)
+// 3. Remote Scanning / BFS Logic
+#define PKT_REPORT_NODE     0x04  // "I found a neighbor: ID Y" (Node reporting to Base)
+#define PKT_SCAN_CMD        0x05  // "Start scanning your area now" (Base commanding Node)
+#define PKT_SCAN_DONE       0x06  // "I finished scanning my area" (Node reporting completion)
 
-#endif
+#endif // PACKET_CONFIG_H
